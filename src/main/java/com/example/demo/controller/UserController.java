5f4fca147d2265aa5dffb0aaabbe25c3b319fc5f
Lucas Diniz <diniz3003@gmail.com>
2022-10-29 15:54:51 -0300

    frist commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDto;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public UserDto registration(@Valid
+                                @RequestBody
+                                @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto) {
+
+        System.out.println(userDto);
+        var user = new UserDto();
+        user.setUserName("DEFAULT");
+        return user;
+
+    }
+
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public UserDto login(@Valid
+                         @RequestBody
+                         @JsonView(UserDto.UserView.Login.class) UserDto userDto) {
+
+        var user = new UserDto();
+        user.setUserName("DEFAULT");
+        return user;
+
+    }
+}
---
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -0,0 +1,37 @@
+package com.example.demo.dto;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
+import org.springframework.lang.NonNull;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class UserDto {
+
+    public interface UserView {
+        public static interface RegistrationPost {}
+        public static interface Login {}
+    }
+
+//    @NotBlank(groups = {UserView.RegistrationPost.class}, message = "UserName não pode estar em branco ou nulo")
+    @JsonView(UserView.RegistrationPost.class)
+    @NotBlank(message = "NÃO DEVE SER VAZIO PORRA")
+    private String userName;
+
+    @NotEmpty(groups = {UserView.Login.class}, message = "Password não pode estar vazio")
+    @JsonView(UserView.Login.class)
+    private Long password;
+
+    @JsonView(UserView.RegistrationPost.class)
+    private String email;
+
+    @JsonView(UserView.RegistrationPost.class)
+    private String cpf;
+
+    private Integer PhoneNumber;
+
+    private String imgProfile;
+}
