72ff4d4328df40e9125ae2ad1fcb2c6d93896d32
Lucas Diniz <diniz3003@gmail.com>
2022-11-06 17:18:06 -0300

    ajuste swagger

--- src/main/java/com/example/demo/configs/SwaggerConfig.java
+++ src/main/java/com/example/demo/configs/SwaggerConfig.java
@@ -7,12 +7,14 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -22,26 +24,22 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
+                .apis(RequestHandlerSelectors.basePackage(
+                        "com.example.demo.controllers"))
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build()
-                .useDefaultResponseMessages(false)
-                .globalResponseMessage(RequestMethod.GET, responseMessageForGET());
+                .build();
     }
 
-    private List<ResponseMessage> responseMessageForGET()
-    {
-        return new ArrayList<ResponseMessage>() {{
-            add(new ResponseMessageBuilder()
-                    .code(500)
-                    .message("500 message")
-                    .responseModel(new ModelRef("Error"))
-                    .build());
-            add(new ResponseMessageBuilder()
-                    .code(403)
-                    .message("Forbidden!")
-                    .build());
-        }};
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "SWAGGER TESTE REST API",
+                "Documentação do REST API",
+                "1.0",
+                null,
+                null,
+                null, null, Collections.emptyList());
     }
 }
